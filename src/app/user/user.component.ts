872727f31489fde4a328dc97c1ffad16c56b9786
mitrasingh <mitra.coder@gmail.com>
2024-07-06 18:15:16 -0400

    update: add user input as object instead of individual inputs for each property

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,16 +8,21 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'users/' + this.avatar;
+    return 'users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
